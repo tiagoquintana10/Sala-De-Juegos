ba0a032816ce30f5f1e00282630f5a84f5723367
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-05-28 17:38:46 -0300

    Update simon-dice.component.ts

--- src/app/componentes/simon-dice/simon-dice.component.ts
+++ src/app/componentes/simon-dice/simon-dice.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment.prod';
+import { Router } from '@angular/router';
+
+const supabase = createClient(environment.apiUrl, environment.publicAnonKey);
 
 @Component({
   selector: 'app-simon-dice',
@@ -8,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class SimonDiceComponent {
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    supabase.auth.getUser().then(({ data: { user }, error }) => {
+      if (error || !user) {
+        this.router.navigate(['/login']);
+      }else{
+        
+      }
+    });
+  }
+  
 }
