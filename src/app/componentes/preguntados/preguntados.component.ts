3f8b95b6d8429a925ebcdb4a95e27dbb71b421f0
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-05-28 17:38:39 -0300

    Update preguntados.component.ts

--- src/app/componentes/preguntados/preguntados.component.ts
+++ src/app/componentes/preguntados/preguntados.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment.prod';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(environment.apiUrl, environment.publicAnonKey);
+
 
 @Component({
   selector: 'app-preguntados',
@@ -7,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrl: './preguntados.component.scss'
 })
 export class PreguntadosComponent {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    supabase.auth.getUser().then(({ data: { user }, error }) => {
+      if (error || !user) {
+        this.router.navigate(['/login']);
+      }else{
+        
+      }
+    });
+  }
 
 }
