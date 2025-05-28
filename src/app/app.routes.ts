e06b759be74fb3860f50984838454b0f534fc016
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-05-28 02:09:02 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ErrorComponent } from './componentes/error/error.component';
 import { Component } from '@angular/core';
 import { RegisterComponent } from './componentes/register/register.component';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { ChatComponent } from './componentes/chat/chat.component';
 
 export const routes: Routes = [
     {
@@ -24,6 +25,10 @@ export const routes: Routes = [
             {
                 path: 'juegos',  // Aquí cargamos el módulo de juegos
                 loadChildren: () => import('./juegos/juegos.module').then(m => m.JuegosModule),
+            },
+            {
+                path: 'chat',
+                component: ChatComponent,
             },
     ],
     },
