a0b36c8b7a3a77ec5e94701b1665b4dc5113ed2b
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-05-29 19:36:09 -0300

    Update cartas.service.ts

--- src/app/services/cartas.service.ts
+++ src/app/services/cartas.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Cartas } from '../models/cartas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartasService {
 
-  constructor() { }
+  private deckId: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  crearMazo(){
+    return this.http.get<{deck_id: string; remaining: number }>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+  }
+
+  setDeckId(id: string) {
+    this.deckId = id;
+  }
+
+  getDeckId(): string {
+    return this.deckId;
+  }
+
+  sacarCarta(){
+    return this.http.get<{ cards: Cartas[]; remaining: number }>(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`);    
+  }
 }
