574e3cc0c24ec2bdaeba9d418c395b2d97ad9194
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-06-04 11:59:54 -0300

    Update preguntados.component.ts

--- src/app/componentes/preguntados/preguntados.component.ts
+++ src/app/componentes/preguntados/preguntados.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment.prod';
 import { createClient } from '@supabase/supabase-js';
+import { PersonajesRYMService } from '../../services/personajes-RYM.service';
+import { Personaje } from '../../models/personajes';
+
 
 const supabase = createClient(environment.apiUrl, environment.publicAnonKey);
 
@@ -12,18 +15,86 @@ const supabase = createClient(environment.apiUrl, environment.publicAnonKey);
   templateUrl: './preguntados.component.html',
   styleUrl: './preguntados.component.scss'
 })
-export class PreguntadosComponent {
+export class PreguntadosComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  usuarioId: string ='';
+  usuarioNombre: string ='';
+
+  personajeCorrecto!: Personaje;
+  opciones: string[] = [];
+  imagenUrl: string = '';
+  respuestaSeleccionada: string = '';
+  respuestaCorrecta: boolean | null = null;
+
+  score: number = 0;
+  ultimoScore: number = 0;
+
+  constructor(private router: Router, private RYMService: PersonajesRYMService  ) {}
 
   ngOnInit(): void {
     supabase.auth.getUser().then(({ data: { user }, error }) => {
       if (error || !user) {
         this.router.navigate(['/login']);
       }else{
-        
+        this.usuarioId = user.id;
+        supabase.from('users-data').select('name').eq('authId',this.usuarioId).single()
+        .then(({data,error}) => {
+          this.usuarioNombre = data?.name || 'Desconocido';
+          this.generarPregunta();  
+        })
       }
     });
   }
 
+  generarPregunta(): void {
+    this.RYMService.getPersonajes().subscribe(data => {
+      const personajes = data.results;
+      
+      // Elegir personaje correcto al azar
+      const aleatorio = personajes[Math.floor(Math.random() * personajes.length)];
+      this.personajeCorrecto = aleatorio;
+      this.imagenUrl = aleatorio.image;
+
+      // Elegir 3 personajes incorrectos
+      const incorrectos = personajes
+        .filter(p => p.name !== aleatorio.name)
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 3)
+        .map(p => p.name);
+
+      // Combinar y mezclar opciones
+      this.opciones = [...incorrectos, aleatorio.name].sort(() => 0.5 - Math.random());
+      this.respuestaSeleccionada = '';
+      this.respuestaCorrecta = null;
+    });
+  }
+
+  seleccionarOpcion(nombre: string): void {
+    this.respuestaSeleccionada = nombre;
+    this.respuestaCorrecta = nombre === this.personajeCorrecto.name;
+
+    if(this.respuestaCorrecta) {
+      this.score++;
+    }else{
+      this.ultimoScore = this.score;
+      this.guardarScore('preguntados',this.ultimoScore);
+      this.score = 0;
+    }
+  }
+
+  guardarScore(juego: string, score:number): void{
+    supabase.from('score-juegos').insert([{
+    user_id: this.usuarioId,
+    nombre: this.usuarioNombre,
+    juego,
+    fecha:new Date().toISOString(),
+    score  
+    }])
+    .then(({error}) => {
+      if(error){
+        console.error('Error al guardar score: ',error.message);
+      }
+    })
+  }
+  
 }
