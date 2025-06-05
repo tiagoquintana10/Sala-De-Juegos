7c781998a7dc884768b2577938c2e587f5d5d382
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2025-06-05 20:04:39 -0300

    Update quien-soy.component.ts

--- src/app/componentes/quien-soy/quien-soy.component.ts
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -17,9 +17,14 @@ export class QuienSoyComponent {
   edad: string = '21 años';
   email: string = 'tiago.exequiel.quintana@gmail.com';
   profesion: string = 'Programador web y deportista';
-  biografia: string = 'Soy programador web con pasión por la tecnología. Me encanta el desarrollo de software utilizando tecnologías como Angular, HTML, CSS y JavaScript.' +
-                      'Fuera del código, soy jugador de futsal de la primera division de Atlanta. Me encanta competir, trabajar en equipo, cualidades que también aplico en el' +
+  biografia: string = 'Soy programador web con pasión por la tecnología. Me encanta el desarrollo de software utilizando tecnologías como Angular, HTML, CSS y JavaScript. ' +
+                      'Fuera del código, soy jugador de futsal de la primera division de Atlanta. Me encanta competir, trabajar en equipo, cualidades que también aplico en el ' +
                       'mundo del desarrollo. Siempre estoy buscando aprender, mejorar y aportar valor tanto en la cancha como en cada proyecto que desarrollo.';
+  miJuego: string = 'Mi juego propuesto es el simon-dice. El juego empieza con un color que se ilumina en la pantalla (por ejemplo, un cuadrado rojo). ' +
+  'Este color es el primer paso de la secuencia de colores. Cada ronda, Simon (el "controlador" del juego) agrega un nuevo color a la secuencia que el jugador debe seguir. ' +
+  'La secuencia se va haciendo más larga a medida que avanzan los niveles del juego. El jugador debe repetir la secuencia de colores haciendo clic sobre los botones correspondientes '+
+  'en el mismo orden en el que se presentaron. Si el jugador acierta, se pasa al siguiente nivel y se agrega otro color a la secuencia. ¿Qué pasa si el jugador se equivoca? ' +
+  'Si el jugador hace clic en un color incorrecto o no sigue la secuencia correctamente, el juego termina y el jugador pierde. El nivel alcanzado se muestra como su puntuación.'
   imagenUrl: string = 'assets/mi-foto.jpg'; 
   
   getAvatarUrl(avatarUrl: string) {
